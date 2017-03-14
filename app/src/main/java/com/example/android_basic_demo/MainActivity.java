034b8972951b5361e58fa1feed12b96d3624ffd0
root <root@yff.com>
2017-03-14 18:54:24 +0800

    window feature no title

--- app/src/main/java/com/example/android_basic_demo/MainActivity.java
+++ app/src/main/java/com/example/android_basic_demo/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.Window;
 import android.widget.ListView;
 
 
@@ -32,6 +33,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.pulltorefresh);
         mAdapter = new ImageListAdapter(this);
         mListView = (PullToRefreshListView) this.findViewById(R.id.pl_refresh);
